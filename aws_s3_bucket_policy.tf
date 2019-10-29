a3df4a122633a8d97d7fac3c47feb16ba91af225
Richard Gutkowski <richard@ososolution.com>
2019-10-29 22:13:54 +0000

    corrected names in resources

--- aws_s3_bucket_policy.tf
+++ aws_s3_bucket_policy.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket_policy" "bucket_policy" {
-  bucket = "${aws_s3_bucket.bucket.id}"
+  bucket = "${aws_s3_bucket.ssm_ansible_bucket.id}"
   policy = <<POLICY
 {
   "Version": "2012-10-17",
@@ -9,8 +9,8 @@ resource "aws_s3_bucket_policy" "bucket_policy" {
       "Principal": "*",
       "Action": "S3:*",
       "Resource": [
-        "${aws_s3_bucket.bucket.arn}/*",
-        "${aws_s3_bucket.bucket.arn}"
+        "${aws_s3_bucket.ssm_ansible_bucket.arn}/*",
+        "${aws_s3_bucket.ssm_ansible_bucket.arn}"
       ],
       "Condition": {
         "Bool": {
