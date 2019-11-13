e360c82167cfd359263661e799e2d465a7fbbffe
Richard Gutkowski <richard@ososolution.com>
2019-11-13 10:58:22 +0000

    wrong bucket used, fixed with this change.

--- aws_ansible_s3.tf
+++ aws_ansible_s3.tf
@@ -1,23 +1,23 @@
 resource "aws_s3_bucket_object" "db_migration_ansible_playbook" {
-  bucket  = "${aws_s3_bucket_object.db_migration_ansible_playbook.id}"
+  bucket  = "${aws_s3_bucket.ssm_ansible_bucket.id}"
   key     = "ansible/openvpn_db_migration_ansible_playbook.yaml"
   content = "${data.template_file.db_migration_ansible_playbook.rendered}"
 }
 
 resource "aws_s3_bucket_object" "db_restore_ansible_playbook" {
-  bucket  = "${aws_s3_bucket_object.db_restore_ansible_playbook.id}"
+  bucket  = "${aws_s3_bucket.ssm_ansible_bucket.id}"
   key     = "ansible/openvpn_db_restore_ansible_playbook.yaml"
   content = "${data.template_file.db_restore_ansible_playbook.rendered}"
 }
 
 resource "aws_s3_bucket_object" "ssl_ansible_playbook" {
-  bucket  = "${aws_s3_bucket_object.ssl_ansible_playbook.id}"
+  bucket  = "${aws_s3_bucket.ssm_ansible_bucket.id}"
   key     = "ansible/openvpn_ssl_ansible_playbook.yaml"
   content = "${data.template_file.ssl_ansible_playbook.rendered}"
 }
 
 resource "aws_s3_bucket_object" "update_ansible_playbook" {
-  bucket  = "${aws_s3_bucket_object.update_ansible_playbook.id}"
+  bucket  = "${aws_s3_bucket.ssm_ansible_bucket.id}"
   key     = "ansible/openvpn_update_ansible_playbook.yaml"
   content = "${data.template_file.update_ansible_playbook.rendered}"
 }
