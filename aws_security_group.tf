8a2556d3687eea86735d0a43d4d92d6635be7963
Richard Gutkowski <richard@ososolution.com>
2019-11-14 15:11:13 +0000

    Added SSH access for private subnet access via VPN

--- aws_security_group.tf
+++ aws_security_group.tf
@@ -5,6 +5,13 @@ resource "aws_security_group" "openvpn-sg" {
 
   # Certbot Access
   ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
